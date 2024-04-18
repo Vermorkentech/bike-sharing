8cdf01eb1f2135541c931db52ca77671f6256119
Ben Runchey <Ben Runchey>
2024-04-18 10:22:01 -0500

    Adding ProcessingGroup annotation to BikeStatusProjection

--- monolith/src/main/java/io/axoniq/bikesharing/query/BikeStatusProjection.java
+++ monolith/src/main/java/io/axoniq/bikesharing/query/BikeStatusProjection.java
@@ -1,11 +1,13 @@
 package io.axoniq.bikesharing.query;
 
 import io.axoniq.bikesharing.api.messages.*;
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("BikeStatus")
 public class BikeStatusProjection {
     
     private final BikeStatusRepository bikeSummaryRepository;
