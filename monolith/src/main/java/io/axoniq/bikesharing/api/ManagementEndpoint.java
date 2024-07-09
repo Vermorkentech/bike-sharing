b64655e8f879547e415495016cd2cb436cd92adf
Christian Vermorken <christian.vermorken@axoniq.io>
2024-07-09 11:46:45 +0200

    Allow reset from specific date

--- monolith/src/main/java/io/axoniq/bikesharing/api/ManagementEndpoint.java
+++ monolith/src/main/java/io/axoniq/bikesharing/api/ManagementEndpoint.java
@@ -1,11 +1,15 @@
 package io.axoniq.bikesharing.api;
 
+import com.google.type.DateTime;
 import io.axoniq.dataprotection.cryptoengine.CryptoEngine;
 import org.axonframework.config.EventProcessingConfiguration;
 import org.axonframework.eventhandling.StreamingEventProcessor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RestController
@@ -21,14 +25,19 @@ public class ManagementEndpoint {
     }
 
     @PostMapping("/eventProcessor/{processorName}/reset")
-    public ResponseEntity<String> resetEventProcessor(@PathVariable String processorName) {
+    public ResponseEntity<String> resetEventProcessor(@PathVariable String processorName, @RequestParam("resetToDate") Optional<String> resetToDate) {
         Optional<StreamingEventProcessor> optionalStreamingEventProcessor =
                 eventProcessingConfiguration.eventProcessor(processorName, StreamingEventProcessor.class);
         if (optionalStreamingEventProcessor.isPresent()) {
             StreamingEventProcessor eventProcessorToReset = optionalStreamingEventProcessor.get();
 
             eventProcessorToReset.shutDown();
-            eventProcessorToReset.resetTokens();
+            if (resetToDate.isPresent()) {
+                var replayFrom = Instant.parse(resetToDate.get());
+                eventProcessorToReset.resetTokens((messageSource) -> messageSource.createTokenAt(replayFrom));
+            } else {
+                eventProcessorToReset.resetTokens();
+            }
             eventProcessorToReset.start();
 
             return ResponseEntity.ok()
